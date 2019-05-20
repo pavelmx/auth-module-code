072ae572837dd23de5877cb206062b0583155871
oadmin <mx-store@inbox.ru>
2019-05-20 11:20:37 +0300

    add register user, entity error

--- src/main/java/com/okta/spring/AuthorizationServerApplication/service/UserServiceImpl.java
+++ src/main/java/com/okta/spring/AuthorizationServerApplication/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.AccountException;
@@ -20,16 +21,14 @@ import java.util.Set;
 @Service
 public class UserServiceImpl implements UserService, UserDetailsService {
 
-
     @Autowired
     UserRepository userRepo;
 
     @Autowired
     RoleRepository roleRepo;
 
-
-//    @Autowired
-//    PasswordEncoder passwordEncoder;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -45,14 +44,16 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             throw new AccountException("Username '" + user.getUsername() + "' already exists");
         }
         Set<Role> roles = new HashSet<>();
+        //for several roles
         /*for (String role: roleNames) {
             roles.add(roleRepo.findByName(role)
                     .orElseThrow(() -> new NotFoundException("Role with name '" + role + "' not found")));
         }*/
+        //for 1 role
         roles.add(roleRepo.findByName(roleNames).get());
         user.setRoles(roles);
         user.setCreated(LocalDateTime.now());
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
         return user;
     }
